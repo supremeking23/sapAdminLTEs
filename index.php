cdd083d010701e0da8daa64926c6ae76a4f22446
Ivan Christian Jay Funcion <ichjfuncion@gmail.com>
2017-08-02 21:48:03 +0800

    first commit

---
+++ index.php
@@ -0,0 +1,490 @@
+
+<!DOCTYPE html>
+<html lang="en">
+	<head>
+	<title>S-APP</title>
+
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+
+	<script type="application/javascript">
+		addEventListener("load", function() { setTimeout(hideURLbar, 0); }, false);
+		function hideURLbar(){ window.scrollTo(0,1); }
+	</script>
+
+
+	<link href="css/bootstrap.css" rel="stylesheet" type="text/css" media="all" />
+	<link href="css/font-awesome.css" rel="stylesheet">
+	<link href="css/flexslider.css" rel="stylesheet" type="text/css" media="screen" property="" />
+	<link href="css/owl.carousel.css" rel="stylesheet">
+	<link href="css/style.css" rel="stylesheet" type="text/css" media="all" />
+	<link href="css/font-style1.css" rel="stylesheet">
+	<link href="css/font-style2.css" rel="stylesheet">
+
+	</head>
+
+	<body>
+	<!-- banner -->
+	<div class="banner" id="home">
+		<div class="banner-overlay-designinfo">
+			<div class="top-header-design">
+				<h1><a class="col-md-4 navbar-brand" href="index.php">S-APP<span>Improving Learning for Students</span></a></h1>
+				<div class="col-md-4 top-header-design-right">
+
+				</div>
+				<div class="col-md-4 top-header-design-left" >
+					<ul class="num-gcubels">
+						<li>
+							<a href="ls/login.php" style="color: #b6cbed; padding-right: 30px;">Login</a>
+							<a href="ls/signup.php" style="color: #b6cbed;">Sign-Up</a>
+						</li>
+					</ul>
+					<div class="gcubels_search">
+						<div class="cd-main-header">
+							<ul class="cd-header-buttons">
+								<li><a class="cd-search-trigger" href="#cd-search"> <span></span></a></li>
+							</ul> <!-- cd-header-buttons -->
+						</div>
+						<div id="cd-search" class="cd-search">
+							<form action="#" method="post">
+								<input name="Search" type="search" placeholder="Search...">
+							</form>
+						</div>
+					</div>
+				</div>
+
+				<div class="clearfix"></div>
+			</div>
+
+			<div class="container">
+				<nav class="navbar navbar-default">
+					<div class="navbar-header navbar-left">
+						<button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1">
+							<span class="sr-only">Toggle navigation</span>
+							<span class="icon-bar"></span>
+							<span class="icon-bar"></span>
+							<span class="icon-bar"></span>
+						</button>
+					</div>
+					<!-- Collect the nav links, forms, and other content for toggling -->
+					<div class="collapse navbar-collapse navbar-right" id="bs-example-navbar-collapse-1" style="margin-left: 300px;">
+						<nav class="link-effect-3" id="link-effect-3">
+							<ul class="nav navbar-nav" >
+								<li class="active"><a href="index.html"><span data-hover="Home">Home</span></a></li>
+								<li><a href="#features-section" > <span data-hover="Features">Features</span></a></li>
+								<li><a href="#developers-section" > <span data-hover="Developers">Developers</span></a></li>
+								<li><a href="#about-section" > <span data-hover="About Us">About Us</span></a></li>
+								<li><a href="#contact-section" > <span data-hover="Contact Us">Contact Us</span></a></li>
+							</ul>
+						</nav>
+					</div>
+				</nav>
+				<div class="gcubel_banner_info">
+					<section class="slider">
+						<div class="flexslider">
+							<ul class="slides">
+								<li>
+									<div class="wthree_banner_info_grid">
+										<h3><span>GCUBE</span>Welcome to <br>s-APP</h3>
+										<p>Where learning takes place</p>
+									</div>
+								</li>
+								<li>
+									<div class="wthree_banner_info_grid">
+										<h3><span>GCUBE</span>Welcome to <br>s-APP</h3>
+										<p>Where learning takes place</p>
+									</div>
+								</li>
+								<li>
+									<div class="wthree_banner_info_grid">
+										<h3><span>GCUBE</span>Welcome to <br>s-APP</h3>
+										<p>Where learning takes place</p>
+									</div>
+								</li>
+								<li>
+									<div class="wthree_banner_info_grid">
+										<h3><span>GCUBE</span>Welcome to <br>s-APP</h3>
+										<p>Where learning takes place</p>
+									</div>
+								</li>
+							</ul>
+						</div>
+					</section>
+				</div>
+			</div>
+		</div>
+	</div>
+	<!-- //banner -->
+
+	<!-- about -->
+	<div id="about-section" class="about-gcubelayouts" style="margin-bottom: 30px; margin-top: 15px;">
+		<div class="container">
+			<div class="col-md-6 col-sm-6 gallery-grids designer gcubelayouts gallery-grids1 wow slideInLeft" style="margin-top: 25px;">
+				<div class="gallery-grid-images designer gcubelayouts" style="margin-top: 0px;">
+					<div class="col-md-4 col-sm-4 gallery-grid gallery-grid-1 history-grid-image">
+						<img src="images/new/finalstudents7.jpg" alt="designer gcubelayouts" class="zoom-img">
+					</div>
+					<div class="col-md-4 col-sm-4 gallery-grid gallery-grid-2 history-grid-image">
+						<img src="images/new/finalstudents2.jpg" alt="designer gcubelayouts" class="zoom-img">
+					</div>
+					<div class="col-md-4 col-sm-4 gallery-grid gallery-grid-3 history-grid-image">
+						<img src="images/new/finalschool.jpg" alt="designer gcubelayouts" class="zoom-img">
+					</div>
+					<div class="col-md-4 col-sm-4 gallery-grid gallery-grid-4 history-grid-image">
+						<img src="images/new/finalstudents.jpg" alt="designer gcubelayouts" class="zoom-img">
+					</div>
+					<div class="col-md-4 col-sm-4 gallery-grid gallery-grid-5 history-grid-image">
+						<img src="images/new/finalstudents5.jpg" alt="designer gcubelayouts" class="zoom-img">
+					</div>
+					<div class="col-md-4 col-sm-4 gallery-grid gallery-grid-6 history-grid-image">
+						<img src="images/new/finalstudents6.jpg" alt="designer gcubelayouts" class="zoom-img">
+					</div>
+					<div class="clearfix"></div>
+				</div>
+			</div>
+
+			<div class="col-md-6 col-sm-6 gallery-grids designer gcubelayouts gallery-grids2 wow slideInRight">
+				<h2 class="tittle-designer-gcubelayouts">We Aim To Improve Education</h2>
+				<h5>LOREM IPSUM DOLOR SIT AMET</h5>
+				<p class="for-gcube-design">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Maecenas pulvinar tellus sed mauvehicula tempor. In hac habitasse platea dictumst. Donec nunc nunc,interdum sed aliquet quis.</p>
+				<p class="for-gcube-design">conditum vitae enim. Quisque molestie consectetur urna quis scelerisque. Morbi at lectus sapien. Donec fgiat arcu in mi placerat ullamcorper.</p>
+			</div>
+			<div class="clearfix"></div>
+
+		</div>
+	</div>
+	<!-- //about -->
+
+	<!--features-->
+	<div id="features-section" class="features-gcube-designer gcubedesigner-ref" style="margin-top: 0px;">
+		<div class="col-md-6 features-left">
+			<div class="feature-grid1" style="margin-top: 40px;">
+				<div class="col-md-4 feature-grids-gcubels">
+					<i class="icon language" aria-hidden="true"></i>
+					<h5>Performance Analysis</h5>
+					<p>Lorem ipsum dolor sit amet </p>
+				</div>
+				<div class="col-md-4 feature-grids-gcubels">
+					<i class="icon cloud" aria-hidden="true"></i>
+					<h5>Vast Online Resources</h5>
+					<p>Lorem ipsum dolor sit amet </p>
+				</div>
+				<div class="col-md-4 feature-grids-gcubels">
+					<i class="icon book" aria-hidden="true"></i>
+					<h5>Tutorial Services</h5>
+					<p>Lorem ipsum dolor sit amet </p>
+				</div>
+				<div class="clearfix"></div>
+			</div>
+			<div class="feature-grid2">
+				<div class="col-md-4 feature-grids-gcubels">
+					<i class="icon group" aria-hidden="true"></i>
+					<h5>Counceling Services</h5>
+					<p>Lorem ipsum dolor sit amet </p>
+				</div>
+				<div class="col-md-4 feature-grids-gcubels">
+					<i class="icon calendar" aria-hidden="true"></i>
+					<h5>Calendar of Events</h5>
+					<p>Lorem ipsum dolor sit amet </p>
+				</div>
+				<div class="col-md-4 feature-grids-gcubels">
+					<i class="icon mail-forward" aria-hidden="true"></i>
+					<h5>Technical Support</h5>
+					<p>Lorem ipsum dolor sit amet </p>
+				</div>
+				<div class="clearfix"></div>
+			</div>
+		</div>
+
+		<div class="col-md-6  features-right">
+			<div class="features-info" align="right">
+				<h3 class="tittle-designer-gcubelayouts white-gcubels" align="center">Our System Features</h3>
+				<p class="for-gcube-design white-gcubels" align="left">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Maecenas pulvinar tellus sed mauvehicula tempor. In hac habitasse platea dictumst. Donec nunc nunc,interdum sed aliquet quis.</p>
+				<a href="signup.html" class="button-gcubelayouts hvr-rectangle-out">Sign-Up Now!</a>
+			</div>
+		</div>
+
+		<div class="clearfix"></div>
+	</div>
+	<!--//features-->
+
+
+	<!-- signup -->
+	<div id="contact-section" class="register-wthree">
+		<div class="container">
+
+			<div class="col-md-6 regstr-r-gcube-designer">
+				<div class="form-bg-gcubels">
+					<h3>Having Trouble Using S-APP?</h3>
+					<p class="for-gcube-design white-gcubels">Our Technical Support is open 24/7. Kindly send your concern to our customer feature via email or contact us through <span><i class="icon phone"> +63 (02)971 338 004</i></span></p>
+					<form action="#" method="post">
+						<input type="email"  name="mailTo" placeholder="To: gcubeltechsupport@umak.edu.ph" required="" />
+						<input type="text"  name="mailSubject" placeholder="Subject: " required="" />
+
+						<textarea name="mailContent" maxlength="500" placeholder="Type Concern Here..." required=""></textarea>
+
+
+						<input type="submit" value="Send" class="button-gcubelayouts hvr-rectangle-out style">
+					</form>
+				</div>
+			</div>
+
+			<div class="col-md-6 regstr-l-gcube-designer">
+				<h3 class="tittle-designer-gcubelayouts white-gcubels"><span></span></h3>
+				<h4></h4>
+				<!--another content-->
+				<section class="examples">
+
+				</section>
+				<div class="clearfix"></div>
+				<!--//another content-->
+			</div>
+			<div class="clearfix"></div>
+		</div>
+	</div>
+	<!-- //signup -->
+
+
+
+	<!-- Developers -->
+	<div id="developers-section" class="test">
+		<div class="container">
+			<div class="col-md-3 test-left-designinfo" align="center">
+				<h3 class="tittle-designer-gcubelayouts">The Development Team</h3>
+			</div>
+			<div class="col-md-9 test-gr">
+				<div class=" test-gri1">
+					<div id="owl-demo2" class="owl-carousel">
+						<div class="design">
+							<div class="test-grid">
+								<div class="col-md-8 test-grid1">
+									<p class="for-gcube-design">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis.
+									Lorem ipsum dolor .</p>
+									<h4>Ely Nell Mojares</h4>
+									<span>Project Manager</span>
+								</div>
+								<div class="col-md-4 test-grid2">
+									<img src="images/new/ely.png" alt="" class="img-r">
+								</div>
+							</div>
+							<div class="clearfix"></div>
+						</div>
+
+						<div class="design">
+							<div class="test-grid">
+								<div class="col-md-8 test-grid1">
+									<p class="for-gcube-design">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis.
+									Lorem ipsum dolor .</p>
+									<h4>Dominic Saronitman</h4>
+									<span>Database Programmer</span>
+								</div>
+								<div class="col-md-4 test-grid2">
+									<img src="images/new/dom.png" alt="" class="img-r">
+								</div>
+							</div>
+							<div class="clearfix"></div>
+						</div>
+
+						<div class="design">
+							<div class="test-grid">
+								<div class="col-md-8 test-grid1">
+									<p class="for-gcube-design">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis.
+									Lorem ipsum dolor.</p>
+									<h4>Ivan Christian Funcion</h4>
+									<span>Server-Side Programmer</span>
+								</div>
+								<div class="col-md-4 test-grid2">
+									<img src="images/new/ivan.png" alt="" class="img-r">
+								</div>
+							</div>
+							<div class="clearfix"></div>
+						</div>
+
+						<div class="design">
+							<div class="test-grid">
+								<div class="col-md-8 test-grid1">
+									<p class="for-gcube-design">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis.
+									Lorem ipsum dolor .</p>
+									<h4>James Villanueva</h4>
+									<span>User-Interface Programmer</span>
+								</div>
+								<div class="col-md-4 test-grid2">
+									<img src="images/new/james.png" alt="" class="img-r">
+								</div>
+							</div>
+							<div class="clearfix"></div>
+						</div>
+
+						<div class="design">
+							<div class="test-grid">
+								<div class="col-md-8 test-grid1">
+									<p class="for-gcube-design">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis.
+									Lorem ipsum dolor .</p>
+									<h4>Alexander Gordo</h4>
+									<span>Assistant Programmer</span>
+								</div>
+								<div class="col-md-4 test-grid2">
+									<img src="images/new/alex.png" alt="" class="img-r">
+								</div>
+							</div>
+							<div class="clearfix"></div>
+						</div>
+
+						<div class="design">
+							<div class="test-grid">
+								<div class="col-md-8 test-grid1">
+									<p class="for-gcube-design">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis.
+									Lorem ipsum dolor .</p>
+									<h4>John Paul Oximina</h4>
+									<span>Lead Data Researcher</span>
+								</div>
+								<div class="col-md-4 test-grid2">
+									<img src="images/new/jp.png" alt="" class="img-r">
+								</div>
+							</div>
+							<div class="clearfix"></div>
+						</div>
+
+						<div class="design">
+							<div class="test-grid">
+								<div class="col-md-8 test-grid1">
+									<p class="for-gcube-design">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis.
+									Lorem ipsum dolor .</p>
+									<h4>Adrian Torero</h4>
+									<span>Assistant Researcher</span>
+								</div>
+								<div class="col-md-4 test-grid2">
+									<img src="images/new/adrian.png" alt="" class="img-r">
+								</div>
+							</div>
+							<div class="clearfix"></div>
+						</div>
+
+					</div>
+				</div>
+			</div>
+		</div>
+	</div>
+	<!-- //Developers -->
+
+
+
+	<!--footer-->
+	<div class="footer gcubelayouts">
+		<div class="container">
+			<div class="footer-row gcubelayouts-design">
+				<div class="col-md-6 footer-grids gcubel-designer">
+					<h6><a href="index.html">S-APP</a></h6>
+					<p class="footer-one-gcubels">
+						Lorem ipsum dolor sit amet, consectetur adipiscing elit. Maecenas pulvinar tellus sed mauvehicula tempor.
+						Lorem ipsum dolor sit amet, consectetur adipiscing elit. Maecenas pulvinar tellus sed mauvehicula tempor.
+					</p>
+					<div class="top-header-design-right">
+						<ul>
+							<li><a href="#"><i class="icon twitter" aria-hidden="true"></i></a></li>
+							<li><a href="#"><i class="icon facebook" aria-hidden="true"></i></a></li>
+							<li><a href="#"><i class="icon linkedin" aria-hidden="true"></i></a></li>
+							<li><a href="#"><i class="icon google" aria-hidden="true"></i></a></li>
+						</ul>
+					</div>
+					<div class="clearfix"></div>
+				</div>
+				<div class="col-md-3 footer-grids gcubel-designer">
+					<h3>Footer Menu</h3>
+					<ul class="b-nav">
+						<li><a href="#home">Home</a></li>
+						<li><a href="about.html" >Features</a></li>
+						<li><a href="blog.html">About Us</a></li>
+					</ul>
+				</div>
+				<div class="col-md-3 footer-grids gcubel-designer">
+					<h3>Contact Info</h3>
+					<p>Makati, Philippines</p>
+					<p>+63 (02)971 338 004</p>
+					<p>J.P. Rizal Ext, Makati, 1215 Metro Manila</p>
+					<p><a href="mailto:info@example.com">info@gcubel.com</a></p>
+				</div>
+
+				<div class="clearfix"> </div>
+			</div>
+		</div>
+			</div>
+		</div>
+	</div>
+	<!--//footer-->
+
+	<!-- copy-right -->
+	<div class="copyright-wthree">
+		<p>&copy; 2017 S-APP . All Rights Reserved</p>
+	</div>
+	<!-- //copy-right -->
+
+	<script type="text/javascript" src="js/jquery-2.1.4.min.js"></script>
+	<!-- flexSlider -->
+	<script defer src="js/jquery.flexslider.js"></script>
+	<script type="text/javascript">
+		$(window).load(function(){
+		  $('.flexslider').flexslider({
+			animation: "slide",
+			start: function(slider){
+			  $('body').removeClass('loading');
+			}
+		  });
+		});
+    </script>
+	<!-- //flexSlider -->
+
+	<!-- requried-jsfiles-for owl -->
+	<script src="js/owl.carousel.js"></script>
+	<script>
+		$(document).ready(function() {
+		  $("#owl-demo2").owlCarousel({
+			items : 1,
+			lazyLoad : false,
+			autoPlay : true,
+			navigation : false,
+			navigationText :  false,
+			pagination : true,
+		  });
+		});
+    </script>
+    <!-- //requried-jsfiles-for owl -->
+
+	<!--search-bar-->
+	<script src="js/main.js"></script>
+	<script type="text/javascript" src="js/move-top.js"></script>
+	<script type="text/javascript" src="js/easing.js"></script>
+	<script type="text/javascript">
+		jQuery(document).ready(function($) {
+			$(".scroll").click(function(event){
+				event.preventDefault();
+				$('html,body').animate({scrollTop:$(this.hash).offset().top},1000);
+			});
+		});
+	</script>
+	<!-- start-smooth-scrolling -->
+	<script type="text/javascript">
+		$(document).ready(function() {
+			$().UItoTop({ easingType: 'easeOutQuart' });
+		});
+	</script>
+
+	<script type="text/javascript">
+	document.onkeydown = function(e) {
+        if (e.ctrlKey &&
+            (e.keyCode === 67 ||
+             e.keyCode === 86 ||
+             e.keyCode === 85 ||
+             e.keyCode === 117)) {
+            alert('not allowed');
+            return false;
+        } else {
+            return true;
+        }
+	};
+	</script>
+	<script src="js/bootstrap.js"></script>
+
+	</body>
+
+</html>
